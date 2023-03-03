2450698f644babbb311aa8676238341af69577e6
efremov-git <anthony.efremov@gmail.com>
2023-03-04 00:19:06 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
-payload = {"name": "User"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
-print(response.text)
+
+response = requests.get("https://playground.learnqa.ru/api/hello", params={"name":"User"})
+parsed_response_text = response.json()
+print(parsed_response_text["answer"])
