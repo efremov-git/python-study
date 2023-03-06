38a753c9f78e5168bd9bd87daf731de02fdf3ebd
efremov-git <anthony.efremov@gmail.com>
2023-03-06 11:00:56 +0800

    Initial commit

--- LearnQA/main_hello(all).py
+++ LearnQA/main_hello(all).py
@@ -40,3 +40,11 @@ print(parsed_response_text["answer"])
 #print(response.text)
 
 
+###headers###
+#import requests
+
+#headers = {"some_header":"123"}
+#response = requests.get("https://playground.learnqa.ru/api/show_all_headers", headers = headers)
+
+#print(response.text)
+#print(response.headers)
