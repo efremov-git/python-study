8b98f2f795387cb7e186faf4424c8599cd6b8d63
efremov-git <anthony.efremov@gmail.com>
2023-03-07 19:28:56 +0800

    Initial commit

--- LearnQA/simple request/main_hello.py
+++
@@ -1,6 +0,0 @@
-###api_hello###
-import requests
-
-response = requests.get("https://playground.learnqa.ru/api/hello", params={"name":"User"})
-parsed_response_text = response.json()
-print(parsed_response_text["answer"])
