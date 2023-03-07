aa0c5f155d76f3cdcc44062a9eddeed80fceec2c
efremov-git <anthony.efremov@gmail.com>
2023-03-07 15:39:35 +0800

    Initial commit

--- LearnQA/pytest/lib/base_case.py
+++ LearnQA/pytest/lib/base_case.py
@@ -1,3 +1,5 @@
+import json.decoder
+
 from requests import Response
 
 class BaseCase:
@@ -8,3 +10,12 @@ class BaseCase:
     def get_header (self, response: Response, headers_name)
         assert headers_name in response.headers, f"Cannot find cookoe with name {headers_name} in the last response"
         return response.headers[headers_name]
+
+    def get_json_value(self, resoponse: Response, name):
+    try:
+        response_as_dict = response.json()
+    except json.decoder.JSONDecodeError
+            assert False, f"Response is not in JSON Format, Response text is '{response.text}'"
+
+        assert  name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+    return response_as_dict[name]
