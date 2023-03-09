e4776f5af509c00774bef9c0c0f3df43a6e062d0
efremov-git <anthony.efremov@gmail.com>
2023-03-09 17:49:32 +0800

    Initial commit

--- learnQA/pytest/tests/test_user_auth.py
+++ learnQA/pytest/tests/test_user_auth.py
@@ -5,15 +5,15 @@ from lib.assertions import Assertions
 
 
 class TestUserAuth(BaseCase):
-   exclude_params = [
-       ("no_cookie"),
-       ("no_token")
-   ]
+    exclude_params = [
+        ("no_cookie"),
+        ("no_token")
+    ]
 
-   def setup(self):
+    def setup(self):
         data = {
-            'email':'vinkotov@example.com',
-            'password':'1234'
+            'email': 'vinkotov@example.com',
+            'password': '1234'
         }
         response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
 
@@ -21,13 +21,12 @@ class TestUserAuth(BaseCase):
         self.token = self.get_header(response1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
-
-   def test_auth_user(self):
+    def test_auth_user(self):
 
         response2 = requests.get(
             "https://playground.learnqa.ru/api/user/auth",
-            headers={"x-csrf-token" : self.token},
-            cookies={"auth_sid" : self.auth_sid}
+            headers={"x-csrf-token": self.token},
+            cookies={"auth_sid": self.auth_sid}
         )
         Assertions.assert_json_value_by_name(
             response2,
@@ -41,8 +40,8 @@ class TestUserAuth(BaseCase):
 
         assert self.user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not leqal to user id from check method"
 
-   @pytest.mark.parametrize('condition', exclude_params)
-   def test_negative_auth_check(self, condition):
+    @pytest.mark.parametrize('condition', exclude_params)
+    def test_negative_auth_check(self, condition):
 
         if condition == "no_cookie":
             response2 = requests.get(
@@ -53,7 +52,7 @@ class TestUserAuth(BaseCase):
         else:
             response2 = requests.get(
                 "https://playground.learnqa.ru/api/user/auth",
-                cookies={"auth_sid" : self.auth_sid}
+                cookies={"auth_sid": self.auth_sid}
             )
         Assertions.assert_json_value_by_name(
             response2,
