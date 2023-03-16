7cb2f0018a6ecdaf65da8a24b305b0c49dff284e
efremov-git <anthony.efremov@gmail.com>
2023-03-16 13:37:17 +0800

    Initial commit

--- Selenium/test1.py
+++ Selenium/test1.py
@@ -2,18 +2,18 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 
-link ="https://rozetka.com.ua/"
+link ="https://avito.ru"
 browser = webdriver.Chrome()
 browser.get(link)
 
-search_string = browser.find_element(By.XPATH, "/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/div/form/div/div/input")
+search_string = browser.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[1]/div/div[3]/div[2]/div[1]/div/div/div/label[1]/input")
 search_string.send_keys("Капибара")
 
-button = browser.find_element(By.XPATH, "/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/div/form/button")
+button = browser.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[1]/div/div[3]/div[2]/div[2]/button/span")
 button.click()
 time.sleep(5)
 
-check = browser.find_element(By.XPATH, "/html/body/app-root/div/div/rz-search/rz-catalog/div/div[1]/rz-search-heading/div/h1").text
+check = browser.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[1]/div/div[3]/div[2]/div[1]/div/div/div/label[1]/input").text
 check2 = "Капибара"
 if check == check2:
     print("test passed")
