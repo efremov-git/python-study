78a2e50f6e510cf0cd2a05d78e01cdf70df9139b
efremov-git <anthony.efremov@gmail.com>
2023-04-24 12:30:16 +0800

    Initial commit

--- Selenium/test2.py
+++ Selenium/test2.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-link ="http://joyreactor.cc/"
+link = "http://joyreactor.cc/"
 browser = webdriver.Chrome()
 browser.maximize_window()
 browser.get(link)
@@ -9,8 +9,7 @@ browser.get(link)
 search_string = browser.find_element(By.ID, "s")
 search_string.send_keys("Капибара")
 
-button = browser.find_element(By.ID, "searchsubmit").click()
-
+browser.find_element(By.ID, "searchsubmit").click()
 
 check = browser.find_element(By.TAG_NAME, "h1").text
 check2 = "Капибара"
